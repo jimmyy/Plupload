185d70e43c9e268efb403425e33afe0d8eb7e489
jimmyy <mystheme@free.fr>
2011-07-22 02:58:31 +0200

    plupload

--- app/controllers/plupload/plUpload.java
+++ app/controllers/plupload/plUpload.java
@@ -5,13 +5,15 @@
  * PlayFramework version 1.2.1
  *
  * @package    controllers.upload
- * @author     Jérémie Robert <mystheme@free.fr>
+ * @author     Jeremie Robert <mystheme@free.fr>
  * @see        plupload
  * 
  */
 package controllers.plupload;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.util.*;
 import play.*;
 import play.mvc.*;
@@ -22,7 +24,40 @@ public class Plupload extends Controller {
     static String targetDir = Play.configuration.getProperty("plupload.uploadDirectory", "../uploads/");
 
     public static void index() {
-        render();
+        File dir = new File(targetDir);
+
+        String[] children = dir.list();
+        if (children == null) {
+            // Either dir does not exist or is not a directory
+        } else {
+            for (int i = 0; i < children.length; i++) {
+                // Get filename of file or directory
+                String filename = children[i];
+            }
+        }
+
+        // It is also possible to filter the list of returned files.
+        // This example does not return any files that start with `.'.
+        FilenameFilter filter = new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return !name.startsWith(".");
+            }
+        };
+        children = dir.list(filter);
+
+        // The list of files can also be retrieved as File objects
+        File[] files = dir.listFiles();
+
+        // This filter only returns directories
+        FileFilter fileFilter = new FileFilter() {
+            public boolean accept(File file) {
+                return file.isDirectory();
+            }
+        };
+        // files = dir.listFiles(fileFilter);
+        files = dir.listFiles();
+
+        render(dir,children, files);
     }
 
     public static void dump() {
@@ -41,10 +76,6 @@ public class Plupload extends Controller {
         String chunks = params.get("chunks");
         String filename = (params.get("name") != null) ? params.get("name") : "";
 
-        System.out.println("chunk : " + chunk);
-        System.out.println("chunks : " + chunks);
-        System.out.println("filename : " + filename);
-
         // Clean the fileName for security reasons
         filename = filename.replaceAll("/[^\\w\\._]+/", "");
 
@@ -60,7 +91,6 @@ public class Plupload extends Controller {
                 count++;
             }
             filename = fileName_a + "_" + count + fileName_b;
-            System.out.println("filename : " + filename);
         }
         // Create target dir
         File t = new File(targetDir);
@@ -70,7 +100,6 @@ public class Plupload extends Controller {
 
         // Look for the content type header
         String contentType = response.getHeader("Content-Type");
-        System.out.println("contentType : " + contentType);
         // Handle non multipart uploads older WebKit versions didn't support multipart in HTML5
         if (contentType != null && contentType.contains("multipart") != false) {
             // Open temp file
@@ -79,15 +108,12 @@ public class Plupload extends Controller {
                 // Read binary input stream and append it to temp file
                 // $in = fopen($_FILES['file']['tmp_name'], "rb");
                 if (file != null) {
-                    System.out.println("rename " + file.getAbsolutePath() + " to " + out.getAbsolutePath());
                     file.renameTo(out);
                 } else {
-                    System.out.println("{\"jsonrpc\" : \"2.0\", \"error\" : {\"code\": 101, \"message\": \"Failed to open input stream.\"}, \"id\" : \"id\"}");
                     return "{\"jsonrpc\" : \"2.0\", \"error\" : {\"code\": 102, \"message\": \"Failed to open output stream.\"}, \"id\" : \"id\"}";
                 }
 
             } else {
-                System.out.println("{\"jsonrpc\" : \"2.0\", \"error\" : {\"code\": 102, \"message\": \"Failed to open output stream.\"}, \"id\" : \"id\"}");
                 return "{\"jsonrpc\" : \"2.0\", \"error\" : {\"code\": 102, \"message\": \"Failed to open output stream.\"}, \"id\" : \"id\"}";
             }
 
@@ -97,21 +123,17 @@ public class Plupload extends Controller {
             // $out = fopen($targetDir.DIRECTORY_SEPARATOR.$fileName, $chunk == 0 ? "wb" : "ab");
             if (out != null) {
                 if (file != null) {
-                    System.out.println("rename " + file.getAbsolutePath() + " to " + out.getAbsolutePath());
                     file.renameTo(out);
                 } else {
-                    System.out.println("{\"jsonrpc\" : \"2.0\", \"error\" : {\"code\": 101, \"message\": \"Failed to open input stream.\"}, \"id\" : \"id\"}");
                     return "{\"jsonrpc\" : \"2.0\", \"error\" : {\"code\": 101, \"message\": \"Failed to open input stream.\"}, \"id\" : \"id\"}";
                 }
 
             } else {
-                System.out.println("{\"jsonrpc\" : \"2.0\", \"error\" : {\"code\": 102, \"message\": \"Failed to open output stream\"}, \"id\" : \"id\"}");
                 return "{\"jsonrpc\" : \"2.0\", \"error\" : {\"code\": 102, \"message\": \"Failed to open output stream\"}, \"id\" : \"id\"}";
             }
         }
 
         // Return JSON-RPC response
-        System.out.println("{\"jsonrpc\" : \"2.0\", \"result\" : null, \"id\" : \"id\"}");
         return "{\"jsonrpc\" : \"2.0\", \"result\" : null, \"id\" : \"id\"}";
     }
 }
